2b9cbd3d19211df1368a06bcabc5e3ec74642cfd
David Yeung <quxbaz@gmail.com>
2015-12-20 19:34:51 -0500

    Up and running.

--- bind.js
+++ bind.js
@@ -1,49 +1,74 @@
 /*
   bind.js
+
+  Uses Range objects to accomplish bindings.
 */
 
-// function has(obj, key) {
-//   return obj.hasOwnProperty(key);
-// }
+var doc = document;
 
-// function each(obj, fn, context) {
-//   if (Array.isArray(obj)) {
-//     for (var i=0; i < obj.length; i++)
-//       fn.call(context, obj, i);
-//   }
-//   else {
-//     for (var k in obj) {
-//       if (obj.hasOwnProperty(k))
-//         fn.call(context, obj[k], k);
-//     }
-//   }
-// }
+function each(obj, fn, context) {
+  if (Array.isArray(obj)) {
+    for (var i=0; i < obj.length; i++)
+      fn.call(context, obj, i);
+  }
+  else {
+    for (var k in obj) {
+      if (obj.hasOwnProperty(k))
+        fn.call(context, obj[k], k);
+    }
+  }
+}
 
-// function extend(a, b) {
-//   if (typeof b != 'object')
-//     return a;
-//   each(b, function(val, key) {
-//     a[key] = val;
-//   });
-//   return a;
-// }
+var setup = function(Handlebars) {
+  Handlebars.registerHelper('bind', function(key) {
+    if (typeof key != 'string')
+      throw new Error('bind function must be passed a string.');
+    return new Handlebars.SafeString(
+      '<script data-bind-to="' + key + '" type="text/placeholder"></script>'
+    );
+  });
+};
 
-// function values(obj) {
-//   var vals = [];
-//   for (var k in obj) {
-//     if (has(obj, k))
-//       vals.push(obj[k]);
-//   }
-//   return vals;
-// }
+var bindProps = function(obj, el) {
+  /*
+    Looks in the DOM element @el for placeholders tags and binds then
+    properties of @obj to them.
 
-// function find(list, pred) {
-//   for (var i=0; i < list.length; i++) {
-//     if (pred(list[i]))
-//       return list[i];
-//   }
-//   return undefined;
-// }
+    <Usage>
+    var v = new View();
+    v.data = 'hello';
+    v.template = '{{bind "data"}}';
+    v.render();
+
+    bind.bindProps(v, v.el); // You must call this after the template
+                             // is rendered into DOM elements.
+
+    console.log(v.el.innerHTML) -> 'hello'
+
+    v.data = 'world';
+    console.log(v.el.innerHTML) -> 'world'
+  */
+  each(el.getElementsByTagName('script'), function(script) {
+    var range = doc.createRange();
+    range.setStartBefore(script);
+    range.setEndAfter(script);
+    var key = script.getAttribute('data-bind-to');
+    range.deleteContents();  // Deletes the script tag.
+    var textNode = doc.createTextNode(obj[key]);
+    range.insertNode(textNode);
+    (function(value) {
+      Object.defineProperty(obj, key, {
+        get: function() {return value},
+        set: function(val) {
+          value = val;
+          textNode.textContent = val;
+        }
+      });
+    })(obj[key]);
+  });
+};
 
 module.exports = {
+  setup: setup,
+  bindProps: bindProps
 };
--- tests/test.js
+++ tests/test.js
@@ -6,4 +6,34 @@ var bind = require('../bind');
 
 describe('bind.js', function() {
 
+  var View;
+
+  before(function() {
+    bind.setup(Handlebars);
+    View = function(data, html) {
+      _.extend(this, data);
+      this.html = html || '';
+      this.el = document.createElement('div');
+      this.$el = $(this.el);
+    };
+    View.prototype.render = function() {
+      this.el.innerHTML = Handlebars.compile(this.html)(this.data);
+      return this;
+    };
+  });
+
+  it("should render a bound property.", function() {
+    var v = new View({foo: 'hello'}, '{{bind "foo"}}').render();
+    bind.bindProps(v, v.el);
+    v.el.innerHTML.should.eql('hello');
+  });
+
+  it("should update a bound property.", function() {
+    var v = new View({foo: 'hello'}, '{{bind "foo"}}').render();
+    bind.bindProps(v, v.el);
+    v.el.innerHTML.should.eql('hello');
+    v.foo = 'world';
+    v.el.innerHTML.should.eql('world');
+  });
+
 });
